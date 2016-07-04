3b0086a711a4067359e8e3df26f704a26bdce62e
Shrey Sharma <shrey.coderclan@gmail.com>
2016-07-04 15:01:05 +0530

    Update usefuls.py
    timecheck() method added

--- usefuls.py
+++ usefuls.py
@@ -5,3 +5,13 @@ def prin(lis):
     for l in lis:
         print(l)
         time.sleep(0.5)
+def timecheck(da,w):
+    m=0
+    if w<3:
+        m=5
+    else:
+        m=4
+    m*=60
+    n=datetime.now()
+    s=(n-da).total_seconds()
+    return int(m-s)
